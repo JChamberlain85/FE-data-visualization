9139d992194949ab02890f03cca12c40ab70922b
mdconfer <michael.david1306@yahoo.com>
2026-02-18 19:12:36 -0600

    Update esp.py
    Finished esp data receive function

--- esp.py
+++ esp.py
@@ -1,4 +1,6 @@
 import socket
+import pandas as pd
+import time
 
 sock = socket.socket()
 
@@ -11,21 +13,25 @@ print(f"Connected to {ip}")
 data_buffer = []
 target_count = 71 # 0 to 70 is 71 numbers
 
-while True:
-    #Send connection message to esp
-    sock.send(b"1") 
-    packet = sock.recv(1024) 
-    
-    if not packet:
-        print("Connection closed by ESP32")
-        break
+# Receives data from the esp32 and appends the data to a csv file
+def receive_data():
+    while True:
+        # Send connection message to esp32
+        time.sleep(0.5)
+        sock.send(b"1") 
+        packet = sock.recv(1024) 
 
-    for byte_val in packet:
-        data_buffer.append(byte_val)
+        if packet == 'f':
+            print('Connection closed by ESP32')
+            break
 
-        # Full set check
-        if len(data_buffer) == target_count:
-            print(f"{data_buffer}")
+        for byte_val in packet:
+            data_buffer.append(byte_val)
 
-            # Reset
-            data_buffer.clear()
+            # Full set check
+            if len(data_buffer) == target_count:
+                df = pd.read_csv('fe_sample.csv')  # Fetch latest price
+                df.loc[-1] = data_buffer
+
+                # Reset
+                data_buffer.clear()
